161a43da62db00790fe8161e52ecdfb9ccb1c826
matiugrin <matiasugrin@gmail.com>
2023-09-12 15:31:10 -0300

    first commit

---
+++ docs/intro.md
@@ -0,0 +1,49 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Welcome To The OpsAdmin API Reference
+
+:::info
+
+The OpsAdmin API is still under heavy development, this documentation is a work in progress.
+
+:::
+
+## Introduction
+
+OpsAdmin is an online software application that creates efficiencies for retail energy suppliers with its marketing, operations, and regulatory-centric tools.
+
+This software is implemented by different retail companies and each one has its own API domain, set of credentials and permission access to certain API methods that will be provided when the participating retail companies start working in the integration.
+
+Since each retail company has its own API domain, a generic one will be shown in the examples throughout the documentation.
+
+It's possible to test the API and see the details of every endpoint (even the ones still not documented here) by accessing the url https://$HOSTNAME/Swagger/index.html
+
+### What's New?
+
+When new features are added to the API is recomended to contact the Retail Energy Company to confirm if any of the new features are going to be used or changed. Usually the these new features and parameters are incorporated in OpsAdmin maintaining the existing system functionality, meaning that if they are not changed, the system will continue operating as before the update. 
+
+If an agent changes the configuration, some endpoints of the API may start failing if the new validations are not met and will require the API user to adapt their applications.
+
+### Release 2023-11
+- This is a test post [Using a Batch File](http://localhost:3000/).
+
+### Release 2023-10
+- Now it's possible to post payment and credit card information using a batch file, see [Using a Batch File](http://localhost:3000/).
+
+### Release 2023-09
+- The [#Get Plans Batch For Wizard](http://localhost:3000/) endpoint no longer allows requesting plans without indicating at least one utility.
+
+### Release 2023-08
+The following properties were added to the [#Get extended information for a single plan](http://localhost:3000/) endpoint:
+- Added new `general_rules` to the plan information: `allow_bypass_credit_check` and `allow_use_date_of_birth` for the credit check process.
+- Added `available_from_date` and `available_until_date` properties to the plan information for plans that need to limit their contract dates to a certain future range.
+- Added `min_start_date_delay_days` property to the plan information for plans that should have their contract start date delayed for a certain number of days in the future.
+
+### Release 2023-06
+- Added display properties to plan documents. See the [#Get Plan Documents](/api#tag/Plans/paths/~1api~1v1~1plans~1docs~1%7BplanId%7D/get) endpoint for details.
+
+### Release 2023-05
+- Added property `customer.opted_marketing_coms` to the [#Create a new enrollment](/api#tag/Enrollments/paths/~1api~1v1~1enrollments~1create/post) endpoint.
---
+++ docusaurus.config.js
@@ -0,0 +1,160 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/matiugrin/docusaurus/tree/master/redocusaurus',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+    [
+      'redocusaurus',
+      {
+        // Plugin Options for loading OpenAPI files
+        specs: [
+          {
+            spec: 'openapi/openapi.yaml',
+            //spec: 'openapi/openapi.json',
+            //spec: 'https://redocly.github.io/redoc/openapi.yaml',
+            route: '/api/',
+          },
+        ],
+        // Theme Options for modifying how redoc renders them
+        theme: {
+          // Change with your site colors
+          primaryColor: 'rgb(52, 109, 219)', //'#1890ff',
+        },
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'OpsAdmin API Reference',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          // {
+          //   type: 'docSidebar',
+          //   sidebarId: 'tutorialSidebar',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          //{to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/api', label: 'Api', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        // links: [
+        //   {
+        //     title: 'Docs',
+        //     items: [
+        //       {
+        //         label: 'OpsAdmin API Reference',
+        //         to: '/',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'Community',
+        //     items: [
+        //       {
+        //         label: 'Stack Overflow',
+        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //       },
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discordapp.com/invite/docusaurus',
+        //       },
+        //       {
+        //         label: 'Twitter',
+        //         href: 'https://twitter.com/docusaurus',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+        //       // {
+        //       //   label: 'Blog',
+        //       //   to: '/blog',
+        //       // },
+        //       {
+        //         label: 'GitHub',
+        //         href: 'https://github.com/facebook/docusaurus',
+        //       },
+        //     ],
+        //   },
+        // ],
+        copyright: `Copyright © ${new Date().getFullYear()}`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
